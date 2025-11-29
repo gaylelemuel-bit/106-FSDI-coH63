d7373921b29627b80fbc9743e21dc5156bd9678b
gaylelemuel.bit <gaylelemuel@gmail.com>
2025-11-29 16:53:53 -0600

    first commit

---
+++ Scripts/dom.js
@@ -0,0 +1,15 @@
+function sayHello(){
+    console.log("Hello World");
+}
+
+function sayGoodbye(){
+    console.log("Goodbye World");
+}
+
+function howAreYou(){
+    console.log("Whatdupp Doh?");
+}
+
+window.omload = sayHello;// excute the function when the page loads -
+//this mens that i wait until the html and the css are loaded before i
+//excute the javaScript
